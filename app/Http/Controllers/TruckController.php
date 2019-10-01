3878ec20379e47fdcc114e61de3ebe10b2f62ad8
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-10-01 17:14:32 +0300

    1 dalis visa

---
+++ app/Http/Controllers/TruckController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Truck;
+use Illuminate\Http\Request;
+use App\Mechanic;
+
+class TruckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $trucks = Truck::all();
+        return view('truck.index', ['trucks' => $trucks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $mechanics = Mechanic::all();
+        return view('truck.create', ['mechanics' => $mechanics]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $truck = new Truck;
+        $truck->maker = $request->truck_maker;
+        $truck->plate = $request->truck_plate;
+        $truck->make_year = $request->truck_make_year;
+        $truck->mechanic_notices = $request->truck_mechanic_notices;
+        $truck->mechanic_id = $request->mechanic_id;
+        $truck->save();
+        return redirect()->route('truck.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Truck $truck)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Truck $truck)
+    {
+        $mechanics = Mechanic::all();
+        return view('truck.edit', ['truck' => $truck, 'mechanics' => $mechanics]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Truck $truck)
+    {
+        $truck->maker = $request->truck_maker;
+        $truck->plate = $request->truck_plate;
+        $truck->make_year = $request->truck_make_year;
+        $truck->mechanic_notices = $request->truck_mechanic_notices;
+        $truck->mechanic_id = $request->mechanic_id;
+        $truck->save();
+        return redirect()->route('truck.index');
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Truck $truck)
+    {
+        $truck->delete();
+        return redirect()->route('truck.index');
+    }
+}
